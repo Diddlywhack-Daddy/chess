b0e2b9f1acec6f72ae3e0f3bda71864d39f001c9
Jacob Larson <jl2379@byu.edu>
2024-10-06 22:11:17 -0600

    Successfully implemented isInCheck

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -1,6 +1,8 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Objects;
 
 /**
  * For a class that can manage a chess game, making moves on a board
@@ -11,6 +13,7 @@ import java.util.Collection;
 public class ChessGame {
     private TeamColor teamTurn = TeamColor.WHITE;
     private ChessBoard board;
+
     public ChessGame() {
         board = new ChessBoard();
         board.resetBoard();
@@ -23,10 +26,14 @@ public class ChessGame {
         return teamTurn;
     }
 
-    private void switchTeams(TeamColor currentTurn){
-        if(currentTurn == TeamColor.WHITE){teamTurn = TeamColor.BLACK;}
-        else{teamTurn = TeamColor.WHITE;}
+    private void switchTeams(TeamColor currentTurn) {
+        if (currentTurn == TeamColor.WHITE) {
+            teamTurn = TeamColor.BLACK;
+        } else {
+            teamTurn = TeamColor.WHITE;
+        }
     }
+
     /**
      * Set's which teams turn it is
      *
@@ -52,8 +59,10 @@ public class ChessGame {
      * startPosition
      */
     public Collection<ChessMove> validMoves(ChessPosition startPosition) {
-        if(board.getPiece(startPosition) == null){return null;}
-        return board.getPiece(startPosition).pieceMoves(board,startPosition);
+        if (board.getPiece(startPosition) == null) {
+            return null;
+        }
+        return board.getPiece(startPosition).pieceMoves(board, startPosition);
     }
 
     /**
@@ -64,11 +73,11 @@ public class ChessGame {
      */
     public void makeMove(ChessMove move) throws InvalidMoveException {
         ChessPosition start = move.getStartPosition();
-        if (validMoves(start).contains(move)){
+        if (validMoves(start).contains(move)) {
             ChessPiece myPiece = board.getPiece(start);
-            board.addPiece(move.getEndPosition(),null);
-            board.addPiece(start,null);
-            board.addPiece(move.getEndPosition(),myPiece);
+            board.addPiece(move.getEndPosition(), null);
+            board.addPiece(start, null);
+            board.addPiece(move.getEndPosition(), myPiece);
         }
     }
 
@@ -79,7 +88,33 @@ public class ChessGame {
      * @return True if the specified team is in check
      */
     public boolean isInCheck(TeamColor teamColor) {
-        throw new RuntimeException("Not implemented");
+        Collection<ChessMove> otherMoves = new ArrayList<>();
+        ChessPosition kingPosition = null;
+        for (int i = 1; i < 9; i++) {
+            for (int j = 1; j < 9; j++) {
+                ChessPosition boardPosition = new ChessPosition(i, j);
+                //safety check for a piece on the square
+                if (board.getPiece(boardPosition) != null) {
+                    //if the piece is the other color, save all their possible moves
+                    if (board.getPiece(boardPosition).getTeamColor() != teamColor) {
+                        otherMoves.addAll(board.getPiece(boardPosition).pieceMoves(board, boardPosition));
+                    }
+                    //checks to see if the piece is the king we are checking, saves his position
+                    else if (board.getPiece(boardPosition).getPieceType() == ChessPiece.PieceType.KING) {
+                        kingPosition = boardPosition;
+                    }
+                }
+            }
+        }
+        int kingRow = kingPosition.getRow();
+        int kingCol = kingPosition.getColumn();
+        for (ChessMove move : otherMoves) {
+            int moveRow = move.getEndPosition().getRow(); int moveCol = move.getEndPosition().getColumn();
+            if (moveRow == kingRow && moveCol == kingCol) {
+                return true;
+            }
+        }
+        return false;
     }
 
     /**
@@ -109,7 +144,7 @@ public class ChessGame {
      * @param board the new board to use
      */
     public void setBoard(ChessBoard board) {
-        throw new RuntimeException("Not implemented");
+        this.board= board;
     }
 
     /**
@@ -118,6 +153,19 @@ public class ChessGame {
      * @return the chessboard
      */
     public ChessBoard getBoard() {
-        throw new RuntimeException("Not implemented");
+        return board;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChessGame chessGame = (ChessGame) o;
+        return teamTurn == chessGame.teamTurn && Objects.equals(board, chessGame.board);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(teamTurn, board);
     }
 }
